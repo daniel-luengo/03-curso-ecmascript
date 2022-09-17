91dbc2e0a258121daa1f14f3de1c7c7ffe3f4c85
Daniel Luengo <daniel.luengo.v@gmail.com>
2022-09-17 17:50:03 -0300

    Clase 4 ecmascript

---
+++ src/es6/01-arrow-function.js
@@ -0,0 +1,13 @@
+
+// Tres tipos de funciones que hacen lo mismo 
+function square (num) {
+    return num * num;
+}
+
+const square = (num) => {
+return num * num;
+}
+
+
+const square = num => num * num;
+
---
+++ src/es6/03-string.js
@@ -0,0 +1,17 @@
+let hello = 'Hello';
+let world = 'World';
+
+
+//Template literals
+let frase = `${hello}  ${world}` ;
+console.log(frase)
+ 
+
+//Multi-line string
+
+let lorem = 'esto es un template string \n' + ' esta es otrta linea'
+console.log(lorem)
+
+let lorem2 = `Esta es una frase epica
+ echa con un enter`
+console.log(lorem2);
