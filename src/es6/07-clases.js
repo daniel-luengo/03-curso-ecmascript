ce1961fbcf0ffcc4afa09b4cf9670979a7cfc6b5
daniel-luengo <dluengo@datview.cl>
2022-09-20 16:19:47 -0300

    instalando paquete json

--- src/es6/07-clases.js
+++ src/es6/07-clases.js
@@ -1,5 +1,7 @@
+//declarando
 class User {};
-
+//instancia de una clase
+//const newUser = new User();
 
 
 class user {
@@ -30,3 +32,53 @@ class user {
 }
 
 const david = new user();
+
+
+//this 
+
+class user {
+    constructor(name) {
+        this.name = name;
+    }
+    //metodos
+    speak() {
+        return 'Hello';
+    }
+    greeting() {
+        return `${this.speak()} ${this.name}`;
+    }
+}
+
+const ana = new user('Ana');
+console.log(ana.greeting())
+
+
+//setters  getters
+
+class user {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    //metodos
+    speak () {
+        return 'Hello';
+    }
+    greeting() {
+        return `${this.speak()} ${this.name}`;
+    }
+
+    get uAge() {
+        return this.age;
+    }
+    set uAge(n) {
+        this.age = n;
+    }
+}
+
+
+
+const bebeloper1 = new user('David'  ,15)
+console.log(bebeloper1.uAge);
+console.log(bebeloper1.uAge = 20)
+
