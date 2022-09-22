fcd6fa85434388acdb10d409d71cadffea90bef5
Daniel Luengo <daniel.luengo.v@gmail.com>
2022-09-22 00:23:41 -0300

    trailing-commas

---
+++ src/es8/03-string-padding.js
@@ -0,0 +1,8 @@
+const string = 'Hello';
+
+console.log(string.padStart(10, 'hi'))
+
+/*En el fondo el resultado de esto seria que nuestro string hello tiene 5
+ elementos, al colocarle el numero 10 lo que hace es rellenar hasta 10 con la palabra "hi" */
+
+ console.log(string.padEnd(6, '_'))
---
+++ src/es8/04-trailing-commas.js
@@ -0,0 +1,7 @@
+const array = [24, 34, 25, 24, , , , ];
+
+console.log(array);
+console.log(array.length);
+
+/*aca lo que se hizo es que se relleno con comas y a su vez estas 
+representam posiciones de elementos vacios*/
