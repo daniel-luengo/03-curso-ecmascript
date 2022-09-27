0d2ae52a3269169ffe3ce67610627e9188492f35
Daniel Luengo <daniel.luengo.v@gmail.com>
2022-09-27 01:24:02 -0300

    Fin del curso

---
+++ src/es13/00-at.js
@@ -0,0 +1,5 @@
+const array = ["one", "two", "three", "four", "five", "six"];
+
+console.log(array[array.length - 1]); //retorna el ultimo elemento de la posicion del array sin saber cuantos elementos tiene.
+
+console.log(array.at(-1))
